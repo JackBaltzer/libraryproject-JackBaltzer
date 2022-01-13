aadc76e0b6eec4dd2b5ba54ea22d9bdb4567fb5c
Jack Baltzer <jabb@tec.dk>
2022-01-13 08:40:12 +0100

    AuthorService implemented

---
+++ LibraryProject.Api/DTOs/AuthorRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryProject.Api.Services
+{
+    public class AuthorRequest
+    {
+        [Required]
+        [StringLength(32, ErrorMessage = "FirstName must not contain more than 32 chars")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(32, ErrorMessage = "LastName must not contain more than 32 chars")]
+        public string LastName { get; set; }
+
+        [StringLength(32, ErrorMessage = "MiddleName must not contain more than 32 chars")]
+        public string MiddleName { get; set; } = "";
+
+        [Required]
+        [Range(1, 2500, ErrorMessage = "Birthyear must be between 1 and 2500")]
+        public int BirthYear { get; set; }
+
+        [Range(1, 2500, ErrorMessage = "YearOfDeath must be between 1 and 2500")]
+        public int? YearOfDeath { get; set; }
+    }
+}
--- LibraryProject.Api/Services/AuthorService.cs
+++ LibraryProject.Api/Services/AuthorService.cs
@@ -10,6 +10,10 @@ namespace LibraryProject.Api.Services
     public interface IAuthorService
     {
         Task<List<AuthorResponse>> GetAllAuthors();
+        Task<AuthorResponse> GetAuthorById(int authorId);
+        Task<AuthorResponse> CreateAuthor(AuthorRequest newAuthor);
+        Task<AuthorResponse> UpdateAuthor(int authorId, AuthorRequest updateAuthor);
+        Task<AuthorResponse> DeleteAuthor(int authorId);
     }
 
     public class AuthorService : IAuthorService
@@ -25,15 +29,84 @@ namespace LibraryProject.Api.Services
         {
             List<Author> authors = await _authorRepository.SelectAllAuthors();
 
-            return authors.Select(author => new AuthorResponse
+            return authors.Select(author => MapAuthorToAuthorResponse(author)).ToList();
+        }
+
+        public async Task<AuthorResponse> GetAuthorById(int authorId)
+        {
+            Author author = await _authorRepository.SelectAuthorById(authorId);
+
+            if (author != null)
             {
-                Id = author.Id,
+                return MapAuthorToAuthorResponse(author);
+            }
+
+            return null;
+        }
+
+        public async Task<AuthorResponse> CreateAuthor(AuthorRequest newAuthor)
+        {
+            Author author = MapAuthorRequstToAuthor(newAuthor);
+
+            Author insertedAuthor = await _authorRepository.InsertNewAuthor(author);
+
+            if(insertedAuthor != null)
+            {
+                return MapAuthorToAuthorResponse(insertedAuthor);
+            }
+
+            return null;
+        }
+
+        public async Task<AuthorResponse> UpdateAuthor(int authorId, AuthorRequest updateAuthor)
+        {
+            Author author = MapAuthorRequstToAuthor(updateAuthor);
+
+            Author updatedAuthor = await _authorRepository.UpdateExistingAuthor(authorId, author);
+
+            if (updatedAuthor != null)
+            {
+                return MapAuthorToAuthorResponse(updatedAuthor);
+            }
+
+            return null;
+        }
+
+        public async Task<AuthorResponse> DeleteAuthor(int authorId)
+        {
+            Author deletedAuthor = await _authorRepository.DeleteAuthorById(authorId);
+
+            if (deletedAuthor != null)
+            {
+                return MapAuthorToAuthorResponse(deletedAuthor);
+            }
+
+            return null;
+        }
+
+        public Author MapAuthorRequstToAuthor(AuthorRequest authorRequest)
+        {
+            return new Author
+            {
+                FirstName = authorRequest.FirstName,
+                LastName = authorRequest.LastName,
+                MiddleName = authorRequest.MiddleName,
+                BirthYear = authorRequest.BirthYear,
+                YearOfDeath = authorRequest.YearOfDeath
+            };
+        }
+
+        public AuthorResponse MapAuthorToAuthorResponse(Author author)
+        {
+            return new AuthorResponse
+            {
+                Id=author.Id,
                 FirstName = author.FirstName,
                 LastName = author.LastName,
                 MiddleName = author.MiddleName,
                 BirthYear = author.BirthYear,
                 YearOfDeath = author.YearOfDeath
-            }).ToList();
+            };
         }
     }
 }
